adf143535f614df11724997c78732bba5f9a5398
bradenpecora <bpecora11@gmail.com>
2022-03-16 02:16:15 -0500

    add basic plotting capabilities

---
+++ data/create_plot.py
@@ -0,0 +1,77 @@
+"""
+Creates plot from outputs of model
+Author: Braden Pecora
+
+In the current version, there are next to no features,
+but the metadata should be fairly easy to access and utilize.
+"""
+import geocoder
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+def main(data, path='data/'):
+    
+    # it seems that geopandas has a built in geocoder, but this was the first thing that worked
+    # maybe its worth changing eventually...
+    locations = {node: geocoder.arcgis(node + ' Texas').latlng for node in data.keys()} # takes ~ 30 seconds
+    for _, latlong in locations.items():
+        latlong.reverse() # is in opposite order of how geopandas will interpret 
+    
+    # clean data
+    def get_relevant_data(nodal_data):
+        outgoing_dicts = nodal_data['distribution']['outgoing']
+        relevant_keys = ['source_class','destination','destination_class']
+        for _, outgoing_dict in outgoing_dicts.items():
+            for key in list(outgoing_dict.keys()):
+                if key not in relevant_keys:
+                    del outgoing_dict[key]
+        return [outgoing_dict for _, outgoing_dict in outgoing_dicts.items()]
+    clean_data = {node: get_relevant_data(nodal_data) for node, nodal_data in data.items() if nodal_data['distribution']['outgoing'] != {}}
+
+    features = []
+    for node, nodal_connections in clean_data.items():
+        node_latlng = locations[node]
+        node_geodata = {
+            'type' : 'Feature',
+            'geometry' : {
+                'type' : 'Point',
+                'coordinates' : node_latlng
+            },
+            'properties' : {
+                'name' : node
+            }
+        }
+        features.append(node_geodata)
+        for nodal_connection in nodal_connections:
+            dest = nodal_connection['destination']
+            dest_latlng = locations[dest]
+            line_geodata = {
+                'type' : 'Feature',
+                'geometry' : {
+                    'type': 'LineString',
+                    'coordinates': [node_latlng, dest_latlng]
+                },
+                'properties' : {
+                    'name': node + ' to ' + dest
+                }
+            }
+            features.append(line_geodata)
+
+    geo_data = {'type': 'FeatureCollection', 'features' : features}
+    distribution = gpd.GeoDataFrame.from_features(geo_data)
+
+    # get Texas plot
+    us_county = gpd.read_file(path + 'US_COUNTY_SHPFILE/US_county_cont.shp')
+    tx_county = us_county[us_county['STATE_NAME'] == 'Texas']
+    tx = tx_county.dissolve()
+
+    # plot
+    base = tx.plot(color='white',edgecolor='black')
+    distribution.plot(ax=base)
+    plt.savefig(path + 'outputs/fig.png')
+
+if __name__ == '__main__':
+    from json import load
+    data = load(open('outputs/outputs.json'))
+    path = ''
+    main(data, path)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from json import load, dump
 from hydrogen_model.hydrogen_v33fuelstationsubsidy import build_h2_model
 from data.generate_outputs import main as generate_outputs
+from data.create_plot import main as create_plot
 
 def main():
+    # trying to make things modular so that it is easier to run only what is needed when working on ML
     inputs = load(open('data/inputs/data.json'))
     nodes_list = [node_data['node'] for node_data in inputs['texas_nodes']]
 
@@ -13,6 +15,8 @@ def main():
     [df.to_csv(output_location + key + '.csv') for key, df in output_dfs.items()]
     with open(output_location + 'outputs.json', 'w', encoding='utf-8') as f:
         dump(output_json, f, ensure_ascii=False, indent=4)
+
+    create_plot(output_json)
     
 if __name__ == '__main__':
     main()
