5f73e66b62706ae5931ddabcc57bcc672869a423
Joshua Rhodes <joshdr83@gmail.com>
2022-03-27 13:26:36 -0500

    added different colors for distribution pathways

--- data/create_plot.py
+++ data/create_plot.py
@@ -8,6 +8,13 @@ but the metadata should be fairly easy to access and utilize.
 import geocoder
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
+import json
+
+## for development 
+#data = json.load(open('outputs/outputs.json'))
+#path='data/'
 
 def main(data, path='data/'):
     
@@ -42,9 +49,13 @@ def main(data, path='data/'):
             }
         }
         features.append(node_geodata)
+
         for nodal_connection in nodal_connections:
+
             dest = nodal_connection['destination']
+            dist_type = nodal_connection['source_class']
             dest_latlng = locations[dest]
+
             line_geodata = {
                 'type' : 'Feature',
                 'geometry' : {
@@ -52,7 +63,8 @@ def main(data, path='data/'):
                     'coordinates': [node_latlng, dest_latlng]
                 },
                 'properties' : {
-                    'name': node + ' to ' + dest
+                    'name': node + ' to ' + dest,
+                    'dist_type': dist_type
                 }
             }
             features.append(line_geodata)
@@ -62,12 +74,33 @@ def main(data, path='data/'):
 
     # get Texas plot
     us_county = gpd.read_file(path + 'US_COUNTY_SHPFILE/US_county_cont.shp')
+    # us_county = gpd.read_file('US_COUNTY_SHPFILE/US_county_cont.shp')
     tx_county = us_county[us_county['STATE_NAME'] == 'Texas']
     tx = tx_county.dissolve()
 
+    nodes = distribution[distribution.type == 'Point']
+    connections = distribution[distribution.type == 'LineString']
+
     # plot
+    node_color = '#219ebc'
+    dist_pipelineLowPurity_col = '#9b2226'
+    dist_truckLiquefied_color = '#fb8500'
+    dist_truckCompressed_color = '#bb3e03'
+
     base = tx.plot(color='white',edgecolor='black')
-    distribution.plot(ax=base)
+    nodes.plot(ax=base, color = node_color)
+    connections[connections['dist_type'] == 'dist_pipelineLowPurity'].plot(ax=base, color = dist_pipelineLowPurity_col)
+    connections[connections['dist_type'] == 'dist_truckLiquefied'].plot(ax=base, color = dist_truckLiquefied_color)
+    connections[connections['dist_type'] == 'dist_truckCompressed'].plot(ax=base, color = dist_truckCompressed_color, legend=True)
+
+    legend_elements = [Line2D([0], [0], color=dist_pipelineLowPurity_col, lw=2, label='Gas Pipeline'), 
+    Line2D([0], [0], color=dist_truckLiquefied_color, lw=2, label='Liquid Truck Route'), 
+    Line2D([0], [0], color=dist_truckCompressed_color, lw=2, label='Gas Truck Route'),
+    Line2D([0], [0], marker='o', color=node_color, label='Node', markerfacecolor=node_color, markersize=5, lw=0)]
+
+    base.legend(handles=legend_elements, loc='upper left')
+
+
     plt.savefig(path + 'outputs/fig.png')
 
 if __name__ == '__main__':
