79a40061fe18be587ea8ce7de8d9328432cb3067
bradenpecora <bpecora11@gmail.com>
2022-04-28 16:20:27 -0500

    Nodes now know their h value

--- data/traceback_path.py
+++ data/traceback_path.py
@@ -1,8 +1,31 @@
 # TODO
-# Goal 1: Get tree of locations - complete
-# Goal 2: match values to get breakdown of where h2 came from
+# Get amounts flowing through edge of tree
+# associate prices with each node
 import json
 from anytree import Node, RenderTree
+
+class MetaNode(Node):
+    """ Class that inherits from anytree.Node and adds additional metadata & methods"""
+    def __init__(self, name, scope=None, parent=None, prod_data = None, dist_data = None, conv_data = None, cons_data = None,):
+        super().__init__(name, parent=parent)
+        self.scope = scope
+
+        self.prod_data = prod_data
+        self.dist_data = dist_data
+        self.conv_data = conv_data
+        self.cons_data = cons_data
+        
+        self.h = self.get_h()
+
+    def get_h(self):
+        if self.dist_data:
+            return self.dist_data['dist_h']
+        elif self.cons_data:
+            return self.cons_data['cons_h']
+        elif self.prod_data:
+            return self.prod_data['prod_h']
+
+        # add price later
 
 def find_children_dist(full_data, parent,current_node,scope='local',past_node = None):
 
@@ -11,13 +34,25 @@ def find_children_dist(full_data, parent,current_node,scope='local',past_node = 
     if scope == 'outgoing':
         destination_class = destination_class.replace(current_node+'_','')
 
-    for _, dist_params in dist_data[scope].items():
+    for dist_name, dist_params in dist_data[scope].items():
         if dist_params['destination_class'] == destination_class:
             if scope == 'outgoing':
                 if dist_params['destination'] != past_node:
                     continue
+
+            if dist_name.startswith("converter"):
+                convertor_class = dist_data['source_class'].replace('convertor_','')
+                conv_data = full_data[current_node]['conversion'][convertor_class]
+            else:
+                conv_data = None
+
                 
-            child = Node(dist_params['source_class'], parent=parent)
+            child = MetaNode(name = dist_params['source_class'],
+                             parent=parent,
+                             scope = scope,
+                             dist_data=dist_params,
+                             conv_data=conv_data,
+                             )
 
             if scope in ['local','outgoing']:
                 find_children_dist(full_data,child,current_node)
@@ -28,7 +63,11 @@ def find_children_dist(full_data, parent,current_node,scope='local',past_node = 
 
 def print_tree(parent):
     for pre, fill, node in RenderTree(parent):
-        print("%s%s" % (pre, node.name))
+        if isinstance(node, MetaNode):
+            h = node.h
+        else:
+            h = None
+        print("%s%s (%s)" % (pre, node.name, h))
 
 
 def main(node, full_data):
@@ -41,8 +80,8 @@ def main(node, full_data):
     parent_node = Node(node)
 
 
-    for consumer in local_data['consumption'].keys():
-        consumer_node = Node(consumer, parent = parent_node)
+    for consumer_name, consumer_data in local_data['consumption'].items():
+        consumer_node = MetaNode(name = consumer_name, parent = parent_node, cons_data=consumer_data, scope='local')
 
         find_children_dist(full_data, consumer_node, node)
 
