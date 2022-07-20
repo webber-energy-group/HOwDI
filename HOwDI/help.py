0710b819b8e3f9306c9807abd4491128756ac91f
bradenpecora <bpecora11@gmail.com>
2022-07-20 15:35:39 -0500

    fix typo

--- HOwDI/help.py
+++ HOwDI/help.py
@@ -7,7 +7,7 @@ HOwDI HELP
 Run the model:          HOwDI run
 Create a figure:        HOwDI create_fig
 Traceback:              HOwDI traceback
-Traceforward:           HOwDI tracefoward
+Traceforward:           HOwDI traceforward
 
 Use the -h flag with any of these options for further info.
     """
