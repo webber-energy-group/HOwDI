2eaebb2050272c3f2ebd758af983bec83b4f8237
bradenpecora <bpecora11@gmail.com>
2022-07-21 11:51:12 -0500

    geocoder now gets county name

--- HOwDI/preprocessing/geocode.py
+++ HOwDI/preprocessing/geocode.py
@@ -1,14 +1,39 @@
 import pandas as pd
 import geopandas as gpd
-import geopy  # necessary installation, unnecessary import
+from geopy.geocoders import Nominatim  # necessary installation, unnecessary import
+
+
+def camel_case_split(str):
+    # adapted from https://www.geeksforgeeks.org/python-split-camelcase-string-to-individual-strings/
+    words = [[str[0]]]
+
+    for c in str[1:]:
+        if words[-1][-1].islower() and c.isupper():
+            words.append(list(c))
+        else:
+            words[-1].append(c)
+
+    words = ["".join(word) for word in words]
+    words[0] = words[0].capitalize()
+    return " ".join(words)
 
 
 def geocode_hubs(file="hubs.csv"):
     """Generates a GeoJSON file from geocoded locations of hub (detailed hubs.csv)"""
+
     hubs = pd.read_csv(file)["hub"].tolist()
+    hubs_cc = [camel_case_split(hub) + ", Texas" for hub in hubs]
+
     geohubs = gpd.tools.geocode(
-        [hub + " Texas" for hub in hubs], provider="arcgis"
+        hubs_cc, provider="nominatim", user_agent="HOwDI"
     ).set_index(pd.Series(hubs, name="hub"))
+
+    geohubs["County"] = [
+        word.lstrip()
+        for words in geohubs["address"].str.split(",")
+        for word in words
+        if ("County") in word
+    ]
 
     return geohubs
 
